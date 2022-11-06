ce09843e9fed6f0a02f629b733daaa2be301a23b
abdul <abdul.karim002@gmail.com>
2022-11-06 14:11:27 +0530

    initial commit

---
+++ TextEditor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextEditor
+//
+//  Created by abdul karim on 06/11/22.
+//
+
+import UIKit
+
+class ViewController: ZSSRichTextEditor {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
